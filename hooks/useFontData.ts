656827fbce90f41d43192848fbdb7266959fa156
baloerrr <akbaradityah444@gmail.com>
2025-04-18 01:50:17 +0700

    add: setup font

--- hooks/useFontData.ts
+++ hooks/useFontData.ts
@@ -2,7 +2,8 @@ import { useFonts } from "expo-font";
 
 export function useFontData() {
     const [fontsLoaded] = useFonts({
-        "Poppins-Bold": require("../assets/fonts/SpaceMono-Regular.ttf"),
+        "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
+        "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
     });
 
     return {
